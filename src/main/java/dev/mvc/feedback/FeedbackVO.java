1b3992566c85cf8d8b0514ac619e3828d02f9945
uo2hy2 <uo2h.y2@gmail.com>
2024-12-12 17:02:22 +0900

    Create feedbackVO.java

---
+++ src/main/java/dev/mvc/feedback/FeedbackVO.java
@@ -0,0 +1,18 @@
+package dev.mvc.feedback;
+
+public class FeedbackVO {
+  /*
+   * "feedbackno" NUMBER(100)   NOT NULL, --피드백 번호
+  "fcontent"  VARCHAR(1000)   NOT NULL, --피드백 내용
+  "rdate" DATE    NOT NULL, -- 작성 날짜
+  "memberno"  NUMBER(100)   NOT NULL --회원 번호 FK
+   */
+
+  /**피드백 번호*/
+  private int feedbackno;
+  /**피드백 내용*/
+  private String fcontent ="";
+  /**작성 날짜*/
+  private String rdate = "";
+  
+}
