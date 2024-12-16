845d83abd1d9d8a25d22bfac5535e8a049bebbb7
syk32 <gs234524@gmail.com>
2024-12-16 18:12:22 +0900

    board cont

--- src/main/java/dev/mvc/board/BoardCont.java
+++ src/main/java/dev/mvc/board/BoardCont.java
@@ -1,5 +1,28 @@
 package dev.mvc.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import dev.mvc.member.MemberProcInter;
+
+@Controller
+@RequestMapping("/board")
 public class BoardCont {
+
+  @Autowired
+  @Qualifier("dev.mvc.board.BoardProc")
+  private BoardProcInter BoardProc;
+  
+  @Autowired
+  @Qualifier("dev.mvc.member.MemberProc") 
+  private MemberProcInter memberProc;
+  
+  public BoardCont() {
+    System.out.println("-> BoardCont created.");
+  }
+  
+  
 
 }
