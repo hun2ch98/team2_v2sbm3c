16c1c5e8b23e3988584a1465c600490840b3d59d
syk32 <gs234524@gmail.com>
2024-12-17 17:25:29 +0900

    Add dev.mvc.board Feature

--- src/main/java/dev/mvc/board/BoardCont.java
+++ src/main/java/dev/mvc/board/BoardCont.java
@@ -1,19 +1,39 @@
 package dev.mvc.board;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import dev.mvc.diary.DiaryProcInter;
+//import dev.mvc.diary.DiaryVOMenu;
+import dev.mvc.diary.DiaryVO;
 import dev.mvc.member.MemberProcInter;
+import dev.mvc.tool.Tool;
+import dev.mvc.tool.Upload;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/board")
+@RequestMapping(value = "/board")
 public class BoardCont {
 
   @Autowired
   @Qualifier("dev.mvc.board.BoardProc")
-  private BoardProcInter BoardProc;
+  private BoardProcInter boardProc;
+  
+  @Autowired
+  @Qualifier("dev.mvc.diary.DiaryProc") 
+  private DiaryProcInter diaryProc;
   
   @Autowired
   @Qualifier("dev.mvc.member.MemberProc") 
@@ -21,6 +41,147 @@ public class BoardCont {
   
   public BoardCont() {
     System.out.println("-> BoardCont created.");
+  }
+  
+  /**
+   * POST 요청시 새로고침 방지, POST 요청 처리 완료 → redirect → url → GET → forward -> html 데이터
+   * 전송
+   * 
+   * @return
+   */
+  @GetMapping(value = "/post2get")
+  public String post2get(Model model, 
+      @RequestParam(name="url", defaultValue="") String url) {
+//    ArrayList<DiaryVOMenu> menu = this.diaryProc.menu();
+//    model.addAttribute("menu", menu);
+
+    return url; // forward, /templates/...
+  }
+  
+  /**
+   * 게시글 생성
+   * @param model
+   * @param BoardVO
+   * @param boardno
+   * @return
+   */
+  @GetMapping(value = "/create")
+  public String create(Model model, 
+      @ModelAttribute("boardVO") BoardVO boardVO, 
+      @RequestParam(name="diaryno", defaultValue="0") int diaryno) {
+//    ArrayList<DiaryVOMenu> menu = this.diaryProc.menu();  // 일기 메뉴 클래스 생성 필요
+//    model.addAttribute("menu", menu);
+
+    DiaryVO diaryVO = this.diaryProc.read(diaryno); // 카테고리 정보를 출력하기위한 목적
+    model.addAttribute("DiaryVO", diaryVO);
+
+    return "/board/create"; // /templates/contents/create.html
+  }
+
+  /**
+   * 처리 
+   * @return
+   */
+  @PostMapping(value = "/create")
+  public String create(HttpServletRequest request, 
+      HttpSession session, 
+      Model model, 
+      @ModelAttribute("boardVO") BoardVO boardVO,
+      RedirectAttributes ra) {
+
+    if (memberProc.isMemberAdmin(session)) { // 관리자로 로그인한경우
+      // ------------------------------------------------------------------------------
+      // 파일 전송 코드 시작
+      // ------------------------------------------------------------------------------
+      String file1 = ""; // 원본 파일명 image
+      String file1saved = ""; // 저장된 파일명, image
+      String thumb1 = ""; // preview image
+
+      String upDir = Board.getUploadDir(); // 파일을 업로드할 폴더 준비
+      // upDir = upDir + "/" + 한글을 제외한 카테고리 이름
+      System.out.println("-> upDir: " + upDir);
+
+      // 전송 파일이 없어도 file1MF 객체가 생성됨.
+      // <input type='file' class="form-control" name='file1MF' id='file1MF'
+      // value='' placeholder="파일 선택">
+      MultipartFile mf = boardVO.getFile1MF();
+
+      file1 = mf.getOriginalFilename(); // 원본 파일명 산출, 01.jpg
+      System.out.println("-> 원본 파일명 산출 file1: " + file1);
+
+      long size1 = mf.getSize(); // 파일 크기
+      if (size1 > 0) { // 파일 크기 체크, 파일을 올리는 경우
+        if (Tool.checkUploadFile(file1) == true) { // 업로드 가능한 파일인지 검사
+          // 파일 저장 후 업로드된 파일명이 리턴됨, spring.jsp, spring_1.jpg, spring_2.jpg...
+          file1saved = Upload.saveFileSpring(mf, upDir);
+
+          if (Tool.isImage(file1saved)) { // 이미지인지 검사
+            // thumb 이미지 생성후 파일명 리턴됨, width: 200, height: 150
+            thumb1 = Tool.preview(upDir, file1saved, 200, 150);
+          }
+
+          boardVO.setFile1(file1); // 순수 원본 파일명
+          boardVO.setFile1saved(file1saved); // 저장된 파일명(파일명 중복 처리)
+          boardVO.setThumb1(thumb1); // 원본이미지 축소판
+          boardVO.setSize1(size1); // 파일 크기
+
+        } else { // 전송 못하는 파일 형식
+          ra.addFlashAttribute("code", "check_upload_file_fail"); // 업로드 할 수 없는 파일
+          ra.addFlashAttribute("cnt", 0); // 업로드 실패
+          ra.addFlashAttribute("url", "/board/msg"); // msg.html, redirect parameter 적용
+          return "redirect:/board/msg"; // Post -> Get - param...
+        }
+      } else { // 글만 등록하는 경우
+        System.out.println("-> 글만 등록");
+      }
+
+      // ------------------------------------------------------------------------------
+      // 파일 전송 코드 종료
+      // ------------------------------------------------------------------------------
+
+      // Call By Reference: 메모리 공유, Hashcode 전달
+      int memberno = (int) session.getAttribute("memberno"); // memberno FK
+      boardVO.setMemberno(memberno);
+      int cnt = this.boardProc.create(boardVO);
+
+      // ------------------------------------------------------------------------------
+      // PK의 return
+      // ------------------------------------------------------------------------------
+      // System.out.println("--> contentsno: " + contentsVO.getContentsno());
+      // mav.addObject("contentsno", contentsVO.getContentsno()); // redirect
+      // parameter 적용
+      // ------------------------------------------------------------------------------
+
+      if (cnt == 1) {
+        // type 1, 재업로드 발생
+        // return "<h1>파일 업로드 성공</h1>"; // 연속 파일 업로드 발생
+
+        // type 2, 재업로드 발생
+        // model.addAttribute("cnt", cnt);
+        // model.addAttribute("code", "create_success");
+        // return "contents/msg";
+
+        // type 3 권장
+        // return "redirect:/contents/list_all"; // /templates/contents/list_all.html
+
+        // System.out.println("-> contentsVO.getCateno(): " + contentsVO.getCateno());
+        // ra.addFlashAttribute("diaryno", contentsVO.getCateno()); // controller ->
+        // controller: X
+
+        ra.addAttribute("memberno", boardVO.getMemberno()); // controller -> controller: O
+        return "redirect:/board/list_by_diaryno";
+
+        // return "redirect:/contents/list_by_cateno?cateno=" + contentsVO.getCateno();
+        // // /templates/contents/list_by_cateno.html
+      } else {
+        ra.addFlashAttribute("code", "create_fail"); // DBMS 등록 실패
+        ra.addFlashAttribute("cnt", 0); // 업로드 실패
+        ra.addFlashAttribute("url", "/board/msg"); // msg.html, redirect parameter 적용
+        return "redirect:/board/msg"; // Post -> Get - param...
+      }
+    } else { // 로그인 실패 한 경우
+      return "redirect:/member/login_cookie_need"; // /member/login_cookie_need.html
+    }
   }
   
   
--- src/main/java/dev/mvc/board/BoardVO.java
+++ src/main/java/dev/mvc/board/BoardVO.java
@@ -1,5 +1,14 @@
 package dev.mvc.board;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
 public class BoardVO {
   /*
    * CREATE TABLE board(
@@ -9,6 +18,9 @@ public class BoardVO {
     rdate           DATE            NOT NULL,
     board_cate      NUMBER(10)      NOT NULL,
     file1           VARCHAR(200)        NULL,
+    file1saved      VARCHAR2(100)    NULL,
+    thumb1          VARCHAR2(100)    NULL,
+    size1           NUMBER(10)         NULL,
     FOREIGN KEY (memberno)  REFERENCES member (memberno)
     );
    */
@@ -19,7 +31,13 @@ public class BoardVO {
   /** 작성 멤버 */
   private Integer memberno;
   
+  /** 게시글 제목*/
+  @NotEmpty(message="제목 입력은 필수 사항입니다.")
+  @Size(min=1, max=50)
+  private String title="";
+  
   /** 게시글 내용*/
+  @NotEmpty(message="내용 입력은 필수 사항입니다.")
   private String bcontent ="";
   
   /** 작성 날짜*/
@@ -28,11 +46,23 @@ public class BoardVO {
   /** 게시글 종류 */
   private int board_cate;
   
-  /** 파일 업로드 */
-  private String file1="";
-  
+  //파일 업로드 관련
+  // -----------------------------------------------------------------------------------
   /**
-   * test
-   */
+  이미지 파일
+  <input type='file' class="form-control" name='file1MF' id='file1MF' 
+             value='' placeholder="파일 선택">
+  */
+  private MultipartFile file1MF = null;
+  /** 메인 이미지 크기 단위, 파일 크기 */
+  private String size1_label = "";
+  /** 메인 이미지 */
+  private String file1 = "";
+  /** 실제 저장된 메인 이미지 */
+  private String file1saved = "";
+  /** 메인 이미지 preview */
+  private String thumb1 = "";
+  /** 메인 이미지 크기 */
+  private long size1 = 0;
 
 }
