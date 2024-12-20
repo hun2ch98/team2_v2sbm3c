d9eee2d69cdb23580d21288206dda138bc6ec176
syk32 <gs234524@gmail.com>
2024-12-20 11:11:42 +0900

    navbar.html

--- src/main/java/dev/mvc/board/BoardProcInter.java
+++ src/main/java/dev/mvc/board/BoardProcInter.java
@@ -73,5 +73,21 @@ public interface BoardProcInter {
    * @return
    */
   public ArrayList<BoardVO> list_by_boardno_search_paging(HashMap<String, Object> map);
+  
+  /** 
+   * SPAN태그를 이용한 박스 모델의 지원, 1 페이지부터 시작 
+   * 현재 페이지: 11 / 22   [이전] 11 12 13 14 15 16 17 18 19 20 [다음] 
+   *
+   * @param cateno 카테고리 번호
+   * @param now_page 현재 페이지
+   * @param word 검색어
+   * @param list_file 목록 파일명
+   * @param search_count 검색 레코드수   
+   * @param record_per_page 페이지당 레코드 수
+   * @param page_per_block 블럭당 페이지 수
+   * @return 페이징 생성 문자열
+   */
+  public String pagingBox(int memberno, int now_page, String word, String list_file, int search_count, 
+      int record_per_page, int page_per_block); 
 
 }
