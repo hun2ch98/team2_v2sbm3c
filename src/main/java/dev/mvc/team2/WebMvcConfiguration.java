e19666d0c449efd5883c28191207e8a2e4f4272d
kdc3246 <baba001109@naver.com>
2024-12-20 10:09:08 +0900

    Add java, html

--- src/main/java/dev/mvc/team2/WebMvcConfiguration.java
+++ src/main/java/dev/mvc/team2/WebMvcConfiguration.java
@@ -1,10 +1,11 @@
 package dev.mvc.team2;
 
+
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import dev.mvc.board.Board;
-
+import dev.mvc.member.Member;
 
 public class WebMvcConfiguration implements WebMvcConfigurer{
   
@@ -15,8 +16,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
       registry.addResourceHandler("/board/storage/**").addResourceLocations("file:///" +  Board.getUploadDir());
       
       System.out.println("Upload Directory: " + Board.getUploadDir());
-
       
+      // C:/kd/deploy/team2/board/storage ->  /board/storage -> http://localhost:9093/board/storage;
+      registry.addResourceHandler("/member/storage/**").addResourceLocations("file:///" +  Member.getUploadDir());
+
   }
 
 }
