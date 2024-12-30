9e5d52a82aabacec4b5e7520ac0ccfb6603ba1bc
kdc3246 <baba001109@naver.com>
2024-12-30 20:46:51 +0900

    Update Member_v1

--- src/main/java/dev/mvc/member/MemberProc.java
+++ src/main/java/dev/mvc/member/MemberProc.java
@@ -38,8 +38,8 @@ public class MemberProc implements MemberProcInter{
   }
  
   @Override
-  public ArrayList<MemberVO> list() {
-    ArrayList<MemberVO> list = this.memberDAO.list(); // 회원 목록 조회
+  public ArrayList<MemberVO> list_all() {
+    ArrayList<MemberVO> list = this.memberDAO.list_all(); // 회원 목록 조회
     return list;
   }
   
@@ -96,24 +96,6 @@ public class MemberProc implements MemberProcInter{
   }
   
   @Override
-  public int update_text(MemberVO memberVO) {
-    int cnt = this.memberDAO.update_text(memberVO);
-    return cnt;
-  }
-  
-  @Override
-  public int update_file(MemberVO memberVO) {
-    int cnt = this.memberDAO.update_file(memberVO);
-    return cnt;
-  }
-  
-  @Override
-  public int delete(int memberno) {
-    int cnt = this.memberDAO.delete(memberno); // 회원 삭제
-    return cnt;
-  }
-  
-  @Override
   public int passwd_check(HashMap<String, Object> map) {
     String passwd = (String)map.get("passwd");
     passwd = this.security.aesEncode(passwd); // 패스워드 AES 암호화
@@ -143,39 +125,27 @@ public class MemberProc implements MemberProcInter{
     
     return cnt;
   }
-  
-  @Override
-  public ArrayList<MemberVO> list_by_memberno_search(HashMap<String, Object> hashMap) {
-      ArrayList<MemberVO> list = this.memberDAO.list_by_memberno_search(hashMap);
-      return list;
-  }
-
-  @Override
-  public int list_by_memberno_search_count(HashMap<String, Object> hashMap) {
-      int cnt = this.memberDAO.list_by_memberno_search_count(hashMap);
-      return cnt;
-  }
 
   @Override
   public ArrayList<MemberVO> list_by_memberno_search_paging(HashMap<String, Object> map) {
-      int begin_of_page = ((int)map.get("now_page") - 1) * Member.RECORD_PER_PAGE;
+    // `now_page`를 기반으로 `startRow`와 `endRow`를 계산합니다.
+    int now_page = (int) map.get("now_page");
+    int record_per_page = 10; // 페이지당 레코드 수
+    
+    int startRow = (now_page - 1) * record_per_page + 1;
+    int endRow = now_page * record_per_page;
 
-      // 시작 rownum 결정
-      int start_num = begin_of_page + 1;
+    // 계산된 값을 `HashMap`에 추가합니다.
+    map.put("startRow", startRow);
+    map.put("endRow", endRow);
 
-      // 종료 rownum
-      int end_num = begin_of_page + Member.RECORD_PER_PAGE;
+    ArrayList<MemberVO> list = this.memberDAO.list_by_memberno_search_paging(map);
 
-      map.put("start_num", start_num);
-      map.put("end_num", end_num);
-
-      ArrayList<MemberVO> list = this.memberDAO.list_by_memberno_search_paging(map);
-
-      return list;
+    return list;
   }
 
   @Override
-  public String pagingBox(int memberno, int now_page, String word, String list_file, int search_count, int record_per_page,
+  public String pagingBox(int memberno, int now_page, String name, String list_file, int search_count, int record_per_page,
         int page_per_block) {
     
       // 전체 페이지 수 계산
@@ -189,72 +159,102 @@ public class MemberProc implements MemberProcInter{
       int start_page = ((now_grp - 1) * page_per_block) + 1;
       int end_page = now_grp * page_per_block;
     
-      // 마지막 페이지를 전체 페이지로 제한
-      end_page = Math.min(end_page, total_page);
+      StringBuffer str = new StringBuffer(); // String class 보다 문자열 추가등의 편집시 속도가 빠름
 
-      StringBuffer pagingHtml = new StringBuffer();
-      pagingHtml.append("<style type='text/css'>");
-      pagingHtml.append("  #paging {text-align: center; margin-top: 5px; font-size: 1em;}");
-      pagingHtml.append("  #paging A:link {text-decoration:none; color:black; font-size: 1em;}");
-      pagingHtml.append("  #paging A:hover{text-decoration:none; background-color: #FFFFFF; color:black; font-size: 1em;}");
-      pagingHtml.append("  #paging A:visited {text-decoration:none;color:black; font-size: 1em;}");
-      pagingHtml.append("  .span_box_1{");
-      pagingHtml.append("    text-align: center;");
-      pagingHtml.append("    font-size: 1em;");
-      pagingHtml.append("    border: 1px;");
-      pagingHtml.append("    border-style: solid;");
-      pagingHtml.append("    border-color: #cccccc;");
-      pagingHtml.append("    padding:1px 6px 1px 6px;");
-      pagingHtml.append("    margin:1px 2px 1px 2px;");
-      pagingHtml.append("  }");
-      pagingHtml.append("  .span_box_2{");
-      pagingHtml.append("    text-align: center;");
-      pagingHtml.append("    background-color: #668db4;");
-      pagingHtml.append("    color: #FFFFFF;");
-      pagingHtml.append("    font-size: 1em;");
-      pagingHtml.append("    border: 1px;");
-      pagingHtml.append("    border-style: solid;");
-      pagingHtml.append("    border-color: #cccccc;");
-      pagingHtml.append("    padding:1px 6px 1px 6px;");
-      pagingHtml.append("    margin:1px 2px 1px 2px;");
-      pagingHtml.append("  }");
-      pagingHtml.append("</style>");
-      pagingHtml.append("<div id='paging'>");
+      // style이 java 파일에 명시되는 경우는 로직에 따라 css가 영향을 많이 받는 경우에 사용하는 방법
+      str.append("<style type='text/css'>");
+      str.append("  #paging {text-align: center; margin-top: 5px; font-size: 1em;}");
+      str.append("  #paging A:link {text-decoration:none; color:black; font-size: 1em;}");
+      str.append("  #paging A:hover{text-decoration:none; background-color: #FFFFFF; color:black; font-size: 1em;}");
+      str.append("  #paging A:visited {text-decoration:none;color:black; font-size: 1em;}");
+      str.append("  .span_box_1{");
+      str.append("    text-align: center;");
+      str.append("    font-size: 1em;");
+      str.append("    border: 1px;");
+      str.append("    border-style: solid;");
+      str.append("    border-color: #cccccc;");
+      str.append("    padding:1px 6px 1px 6px; /*위, 오른쪽, 아래, 왼쪽*/");
+      str.append("    margin:1px 2px 1px 2px; /*위, 오른쪽, 아래, 왼쪽*/");
+      str.append("  }");
+      str.append("  .span_box_2{");
+      str.append("    text-align: center;");
+      str.append("    background-color: #668db4;");
+      str.append("    color: #FFFFFF;");
+      str.append("    font-size: 1em;");
+      str.append("    border: 1px;");
+      str.append("    border-style: solid;");
+      str.append("    border-color: #cccccc;");
+      str.append("    padding:1px 6px 1px 6px; /*위, 오른쪽, 아래, 왼쪽*/");
+      str.append("    margin:1px 2px 1px 2px; /*위, 오른쪽, 아래, 왼쪽*/");
+      str.append("  }");
+      str.append("</style>");
+      str.append("<div id='paging'>");
       
-      pagingHtml.append("<style type='text/css'>");
-      // CSS 스타일 설정
-      pagingHtml.append("</style>");
-      pagingHtml.append("<DIV id='paging'>");
-
+      // 이전 그룹 링크
       int _now_page = (now_grp - 1) * page_per_block;
-      if (now_grp >= 2) {
-        pagingHtml.append("<span class='span_box_1'><A href='" + list_file + "?word=" + word + "&now_page=" + _now_page
-              + "'>이전</A></span>");
-      }
+      if (now_grp >= 2) { // 현재 그룹번호가 2이상이면 페이지수가 11페이지 이상임으로 이전 그룹으로 갈수 있는 링크 생성
 
+        str.append("<span class='span_box_1'><A href='" + list_file + "?name=" + name + "&now_page=" + _now_page
+            + "'>이전</A></span>");
+      }
+      
+      // 중앙의 페이지 목록
       for (int i = start_page; i <= end_page; i++) {
-          if (i > total_page) {
-              break;
-          }
-
-          if (now_page == i) {
-            pagingHtml.append("<span class='span_box_2'>" + i + "</span>"); // 현재 페이지 강조
-          } else {
-            pagingHtml.append("<span class='span_box_1'><A href='" + list_file + "?word=" + word + "&now_page=" + i + "'>" + i
-                  + "</A></span>");
-          }
+        if (i > total_page) { // 마지막 페이지를 넘어갔다면 페이 출력 종료
+          break;
+        }
+        
+        if (now_page == i) { // 목록에 출력하는 페이지가 현재페이지와 같다면 CSS 강조(차별을 둠)
+          str.append("<span class='span_box_2'>" + i + "</span>"); // 현재 페이지, 강조
+        } else {
+          // 현재 페이지가 아닌 페이지는 이동이 가능하도록 링크를 설정
+          str.append("<span class='span_box_1'><A href='" + list_file + "?name=" + name + "&now_page=" + i + "'>" + i
+              + "</A></span>");
+        }
       }
-
-      _now_page = (now_grp * page_per_block) + 1;
+      
+      // 10개 다음 페이지로 이동
+      // nowGrp: 1 (1 ~ 10 page), nowGrp: 2 (11 ~ 20 page), nowGrp: 3 (21 ~ 30 page)
+      // 현재 페이지 5일경우 -> 현재 1그룹: (1 * 10) + 1 = 2그룹의 시작페이지 11
+      // 현재 페이지 15일경우 -> 현재 2그룹: (2 * 10) + 1 = 3그룹의 시작페이지 21
+      // 현재 페이지 25일경우 -> 현재 3그룹: (3 * 10) + 1 = 4그룹의 시작페이지 31
+      _now_page = (now_grp * page_per_block) + 1; // 최대 페이지수 + 1
       if (now_grp < total_grp) {
-        pagingHtml.append("<span class='span_box_1'><A href='" + list_file + "?word=" + word + "&now_page=" + _now_page
-              + "'>다음</A></span>");
+        str.append("<span class='span_box_1'><A href='" + list_file + "?name=" + name + "&now_page=" + _now_page
+            + "'>다음</A></span>");
       }
-      pagingHtml.append("</DIV>");
+      str.append("</div>");
 
-      return pagingHtml.toString();
+      return str.toString();
+  }
+  
+  @Override
+  public ArrayList<MemberVO> list_by_memberno_search(HashMap<String, Object> hashMap) {
+      ArrayList<MemberVO> list = this.memberDAO.list_by_memberno_search(hashMap);
+      return list;
   }
 
-
+  @Override
+  public int list_by_memberno_search_count(HashMap<String, Object> hashMap) {
+      int cnt = this.memberDAO.list_by_memberno_search_count(hashMap);
+      return cnt;
+  }
   
+  @Override
+  public int update_text(MemberVO memberVO) {
+    int cnt = this.memberDAO.update_text(memberVO);
+    return cnt;
+  }
+  
+  @Override
+  public int update_file(MemberVO memberVO) {
+    int cnt = this.memberDAO.update_file(memberVO);
+    return cnt;
+  }
+  
+  @Override
+  public int delete(int memberno) {
+    int cnt = this.memberDAO.delete(memberno); // 회원 삭제
+    return cnt;
+  }
 }
