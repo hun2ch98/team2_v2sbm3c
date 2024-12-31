a9dc82db58cf3bc74e48bf146f4fb86a818b22aa
hun2ch98 <hun2.ch98@gmail.com>
2024-12-31 18:11:53 +0900

    fixed: illustration features

--- src/main/java/dev/mvc/illustration/IllustrationDAOInter.java
+++ src/main/java/dev/mvc/illustration/IllustrationDAOInter.java
@@ -7,13 +7,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Repository;
 
 import dev.mvc.diary.DiaryVO;
 
 @Mapper
-@Repository
 public interface IllustrationDAOInter {
 
     int create(IllustrationVO illustrationVO);
@@ -30,21 +27,18 @@ public interface IllustrationDAOInter {
 
     int delete(int illustno);
 
-    int list_by_illustno_search_count(HashMap<String, Object> hashMap);
+    int list_by_illustno_search_count(Map<String, Object> paramMap);
 
-    ArrayList<IllustrationVO> list_by_illustno_search_paging(HashMap<String, Object> map);
+    ArrayList<IllustrationVO> list_by_illustno_search_paging(Map<String, Object> paramMap);
 
     int count_by_illustno(int illustno);
 
     Date getDiaryDateByIllustNo(int illustno);
-    
-    /**
-     * 검색어와 페이지 번호에 따라 일러스트 목록을 가져옴
-     * @param map
-     * @return 일러스트 목록
-     */
-    ArrayList<IllustrationVO> listByIllustNoSearchPaging(HashMap<String, Object> map);
-    
-    public List<DiaryVO> listByDateRange(Map<String, Object> paramMap);
 
+    ArrayList<IllustrationVO> listByIllustNoSearchPaging(HashMap<String, Object> paramMap);
+
+    int searchCount(Map<String, Object> paramMap);
+
+    int countByDateRange(HashMap<String, Object> paramMap);
+    
 }
