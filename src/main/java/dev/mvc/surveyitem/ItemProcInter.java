c429407cd385ad51c7285b13b272a54a9d336f9f
syk32 <gs234524@gmail.com>
2025-01-03 20:03:22 +0900

    surveyitem list search, paging

--- src/main/java/dev/mvc/surveyitem/ItemProcInter.java
+++ src/main/java/dev/mvc/surveyitem/ItemProcInter.java
@@ -1,6 +1,7 @@
 package dev.mvc.surveyitem;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public interface ItemProcInter {
   
@@ -26,13 +27,6 @@ public interface ItemProcInter {
   public ArrayList<ItemVO> list_all_com(int surveyno);
   
   /**
-   * 관리자
-   * 목록
-   * @return
-   */
-  public ArrayList<ItemVO> admin_list(int surveyno);
-  
-  /**
    * 수정
    * @param itemVO
    * @return
@@ -51,6 +45,42 @@ public interface ItemProcInter {
    * @param itemVO
    * @return
    */
-  public int update_cnt(int itemVO);
+  public int update_cnt(int itemno);
+  
+  /**
+   * 검색 목록
+   * @return
+   */
+  public ArrayList<ItemVO> list_search(String word);
+  
+  /**
+   * 검색 갯수
+   * @param word
+   * @return
+   */
+  public Integer count_by_search(String word);
+  
+  /**
+   * 검색 + 페이징 목록
+   * @param map
+   * @return
+   */
+  public ArrayList<ItemVO> list_search_paging(int surveyno, String word, int now_page, int record_per_page);
+  
+  /** 
+   * SPAN태그를 이용한 박스 모델의 지원, 1 페이지부터 시작 
+   * 현재 페이지: 11 / 22   [이전] 11 12 13 14 15 16 17 18 19 20 [다음] 
+   *
+   * @param now_page  현재 페이지
+   * @param word 검색어
+   * @param list_file 목록 파일명
+   * @param search_count 검색 레코드수   
+   * @param record_per_page 페이지당 레코드 수
+   * @param page_per_block 블럭당 페이지 수
+   * @return 페이징 생성 문자열
+   */
+  String pagingBox(int surveyno, int now_page, String word, String list_file_name, int search_count, int record_per_page,
+      int page_per_block);
+  
 
 }
