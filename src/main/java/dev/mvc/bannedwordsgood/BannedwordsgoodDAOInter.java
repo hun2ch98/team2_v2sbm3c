3c03b83bfea7c6d08ff8143aba1a1d99f2ba8f98
uo2hy2 <uo2h.y2@gmail.com>
2025-01-07 12:28:48 +0900

    Update Bannedwordsgood

---
+++ src/main/java/dev/mvc/bannedwordsgood/BannedwordsgoodDAOInter.java
@@ -0,0 +1,11 @@
+package dev.mvc.bannedwordsgood;
+
+public interface BannedwordsgoodDAOInter {
+  /**
+   * 등록, 추상 메소드
+   * @param contentsgoodVO
+   * @return
+   */
+  public int create(BannedwordsgoodVO bannedwordsgoodVO);
+  
+}
---
+++ src/main/java/dev/mvc/bannedwordsgood/BannedwordsgoodProcInter.java
@@ -0,0 +1,12 @@
+package dev.mvc.bannedwordsgood;
+
+public interface BannedwordsgoodProcInter {
+	
+	 /**
+	   * 등록, 추상 메소드
+	   * @param bannedwordsgoodVO
+	   * @return
+	   */
+	  public int create(BannedwordsgoodVO bannedwordsgoodVO);
+
+}
