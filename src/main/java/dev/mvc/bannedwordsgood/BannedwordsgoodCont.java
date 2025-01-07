9d32e989e751ce376cae9414ae03a06c132c6a73
uo2hy2 <uo2h.y2@gmail.com>
2025-01-07 17:01:22 +0900

    Update bannedwordsgood

--- src/main/java/dev/mvc/bannedwordsgood/BannedwordsgoodCont.java
+++ src/main/java/dev/mvc/bannedwordsgood/BannedwordsgoodCont.java
@@ -11,13 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import dev.mvc.member.MemberProcInter;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping(value = "/bannedwordsgood")
 public class BannedwordsgoodCont {
+  @Autowired
+  @Qualifier("dev.mvc.member.MemberProc") // @Service("dev.mvc.member.MemberProc")
+   private MemberProcInter memberProc;
+  
   @Autowired
   @Qualifier("dev.mvc.bannedwordsgood.BannedwordsgoodProc")
   BannedwordsgoodProcInter bannedwordsgoodProc;
@@ -25,7 +32,22 @@ public class BannedwordsgoodCont {
   public BannedwordsgoodCont() {
 	System.out.println("-> BannedwordsgoodCont created.");
   }
-	
+  
+  /**
+   * POST 요청시 새로고침 방지, POST 요청 처리 완료 → redirect → url → GET → forward -> html 데이터
+   * 전송
+   * 
+   * @return
+   */
+  @GetMapping(value = "/post2get")
+  public String post2get(Model model, 
+      @RequestParam(name="url", defaultValue = "") String url) {
+//    ArrayList<CateVOMenu> menu = this.cateProc.menu();
+//    model.addAttribute("menu", menu);
+
+    return url; // forward, /templates/...
+  }
+  
   @PostMapping(value= "/create")
   @ResponseBody
   public String create(HttpSession session, @RequestBody BannedwordsgoodVO bannedwordsgoodVO) { 
@@ -58,6 +80,29 @@ public class BannedwordsgoodCont {
 //	    model.addAttribute("menu", menu);
 
     return "/bannedwordsgood/list_all"; // /templates/bannedwordsgood/list_all.html
+  }
+  
+  /**
+   * 삭제 처리
+   * 
+   * @return
+   */
+  @PostMapping(value = "/delete")
+  public String delete_proc(HttpSession session, 
+      Model model, 
+      @RequestParam(name="goodno", defaultValue = "0") int goodno, 
+      RedirectAttributes ra) {    
+    
+    if (this.memberProc.isMemberAdmin(session)) { // 관리자 로그인 확인
+      this.bannedwordsgoodProc.delete(goodno);
+
+      return "redirect:/bannedwordsgood/list_all";
+
+    } else { // 정상적인 로그인이 아닌 경우 로그인 유도
+      ra.addAttribute("url", "/member/login_cookie_need"); // /templates/member/login_cookie_need.html
+      return "redirect:/bannedwordsgood/post2get"; // @GetMapping(value = "/msg")
+    }
+
   }
   
 	
