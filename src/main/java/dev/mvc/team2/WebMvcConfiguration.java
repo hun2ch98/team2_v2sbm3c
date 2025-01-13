710f43f5d0e7d8f6ff6c4049b6f2d28255c307a0
hun2ch98 <hun2.ch98@gmail.com>
2025-01-13 13:07:27 +0900

    fixed diary create feature

--- src/main/java/dev/mvc/team2/WebMvcConfiguration.java
+++ src/main/java/dev/mvc/team2/WebMvcConfiguration.java
@@ -18,29 +18,35 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     
-      // C:/kd/deploy/team2/board/storage ->  /board/storage -> http://localhost:9093/board/storage;
-      registry.addResourceHandler("/board/storage/**").addResourceLocations("file:///" +  Board.getUploadDir());
-      
-      // C:/kd/deploy/team2/illustration/storage ->  /illustration/storage -> http://localhost:9093/grade/storage;
-      registry.addResourceHandler("/upload/**") // HTTP 요청 경로
-      .addResourceLocations("file:C:/kd/deploy/team2/illustration/storage/");
-      
-      // C:/kd/deploy/team2/survey/storage ->  /survey/storage -> http://localhost:9093/survey/storage;
-      registry.addResourceHandler("/survey/storage/**").addResourceLocations("file:///" +  Survey.getUploadDir());
-      
-      System.out.println("Upload Directory: " + Board.getUploadDir());
-      
-      // C:/kd/deploy/team2/member/storage ->  /board/storage -> http://localhost:9093/board/storage;
-      registry.addResourceHandler("/member/storage/**").addResourceLocations("file:///" +  Member.getUploadDir());
-      
-      // C:/kd/deploy/team2/grade/storage ->  /board/storage -> http://localhost:9093/grade/storage;
-      registry.addResourceHandler("/grade/storage/**").addResourceLocations("file:///" +  Grade.getUploadDir());
+    registry.addResourceHandler("/emotion/images/**")
+    .addResourceLocations("classpath:/static/emotion/images/");
+    
+    registry.addResourceHandler("/weather/images/**")
+    .addResourceLocations("classpath:/static/weather/images/");
+  
+    // C:/kd/deploy/team2/board/storage ->  /board/storage -> http://localhost:9093/board/storage;
+    registry.addResourceHandler("/board/storage/**").addResourceLocations("file:///" +  Board.getUploadDir());
+    
+    // C:/kd/deploy/team2/illustration/storage ->  /illustration/storage -> http://localhost:9093/grade/storage;
+    registry.addResourceHandler("/upload/**") // HTTP 요청 경로
+    .addResourceLocations("file:C:/kd/deploy/team2/illustration/storage/");
+    
+    // C:/kd/deploy/team2/survey/storage ->  /survey/storage -> http://localhost:9093/survey/storage;
+    registry.addResourceHandler("/survey/storage/**").addResourceLocations("file:///" +  Survey.getUploadDir());
+    
+    System.out.println("Upload Directory: " + Board.getUploadDir());
+    
+    // C:/kd/deploy/team2/member/storage ->  /board/storage -> http://localhost:9093/board/storage;
+    registry.addResourceHandler("/member/storage/**").addResourceLocations("file:///" +  Member.getUploadDir());
+    
+    // C:/kd/deploy/team2/grade/storage ->  /board/storage -> http://localhost:9093/grade/storage;
+    registry.addResourceHandler("/grade/storage/**").addResourceLocations("file:///" +  Grade.getUploadDir());
 
-      // C:/kd/deploy/team2/grade/storage ->  /grade/storage -> http://localhost:9093/grade/storage;
-      registry.addResourceHandler("/emotion/storage/**").addResourceLocations("file:///" +  Emotion.getUploadDir());
+    // C:/kd/deploy/team2/grade/storage ->  /grade/storage -> http://localhost:9093/grade/storage;
+    registry.addResourceHandler("/emotion/storage/**").addResourceLocations("file:///" +  Emotion.getUploadDir());
 
-      // C:/kd/deploy/team2/grade/storage ->  /grade/storage -> http://localhost:9093/grade/storage;
-      registry.addResourceHandler("/weather/storage/**").addResourceLocations("file:///" +  Weather.getUploadDir());
+    // C:/kd/deploy/team2/grade/storage ->  /grade/storage -> http://localhost:9093/grade/storage;
+    registry.addResourceHandler("/weather/storage/**").addResourceLocations("file:///" +  Weather.getUploadDir());
   }
 
 }
